17cefcd01f9b9ca85e9cefc6827694f87a876f58
Tom Hennessy <tomlhennessy@outlook.com>
2024-06-23 09:37:35 +0100

    monkey patched array class

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,15 @@
-// Your code here
+Array.prototype.isEqual = function(array) {
+    // check if the lengths are different
+    if (this.length !== array.length) {
+        return false;
+    }
+
+    // compare each element
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] !== array[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
